15c34717c760b73fd48ef756fba32346bcdc1fe4
Kevin Finnigin <kevin@finnigin.net>
2024-01-23 07:18:41 -0500

    update license

--- cmake/install_directives.cmake
+++ cmake/install_directives.cmake
@@ -1,6 +1,6 @@
 # Copyright (c) 2021-present, The osquery authors
 #
-# This source code is licensed as defined by the LICENSE file found in the
+# This source code is licensed as defined by the LICENSE-Osquery file found in the
 # root directory of this source tree.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
